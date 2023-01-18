fd01a5dcdda454997dc21f07db4098cc3b00e8b0
laurenthat <116711536+laurenthat@users.noreply.github.com>
2023-01-18 10:17:58 +0200

    doFetch

--- hooks/ApiHooks.js
+++ hooks/ApiHooks.js
@@ -1,6 +1,18 @@
 import {useEffect, useState} from 'react';
 import {baseUrl} from '../utils/variables';
 
+const doFetch = async (url, options) => {
+  const response = await fetch(url, options);
+  const json = await response.json(); // or return await response.json();
+  if (!response.ok) {
+    const message = json.error
+      ? `${json.message}: ${json.error}`
+      : json.message;
+    throw new Error(message || response.statusText);
+  }
+  return json;
+};
+
 const useMedia = () => {
   const [mediaArray, setMediaArray] = useState([]);
 
@@ -26,4 +38,29 @@ const useMedia = () => {
   return {mediaArray}; // it's befind brackets because it is equal to {mediaArray: mediaArray}
 };
 
-export {useMedia};
+const useAuthentication = () => {
+  const postLogin = async (userCredentials) => {
+    // user credentials format: {username: 'someUsername', password: 'somePassword'}
+    const options = {
+      // TODO: add method, headers and body for sending json data with POST
+    };
+    try {
+      // TODO: use fetch to send request to login endpoint and return the result as json, handle errors with try/catch and response.ok
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+
+  return {postLogin};
+};
+
+// https://media.mw.metropolia.fi/wbma/docs/#api-User
+const useUser = () => {
+  const checkUser = async () => {
+    // https://media.mw.metropolia.fi/wbma/docs/#api-User-CheckUserName
+  };
+  // delete user const deleteUser
+  // add a new user const addUser
+};
+
+export {useMedia, useAuthentication, useUser};
