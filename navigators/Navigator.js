6fe10db73d23511f937674508600190d45643f98
laurenthat <116711536+laurenthat@users.noreply.github.com>
2023-01-30 09:31:06 +0200

    add ypload view to navigator

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -8,6 +8,8 @@ import Single from '../views/Single';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {MainContext} from '../contexts/MainContext';
 import Login from '../views/Login';
+import Upload from '../views/Upload';
+import {Icon} from '@rneui/base';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -15,23 +17,42 @@ const Stack = createNativeStackNavigator();
 const TabScreen = () => {
   return (
     <Tab.Navigator
-      screenOptions={({route}) => {
-        return {
-          tabBarIcon: ({focused, color, size}) => {
-            let iconName;
-            if (route.name === 'Home') {
-              iconName = 'home';
-            } else if (route.name === 'Profile') {
-              iconName = 'person';
-            }
+    // screenOptions={({route}) => {
+    //   return {
+    //     tabBarIcon: ({focused, color, size}) => {
+    //       let iconName;
+    //       if (route.name === 'Home') {
+    //         iconName = 'home';
+    //       } else if (route.name === 'Profile') {
+    //         iconName = 'person';
+    //       }
 
-            return <Ionicons name={iconName} />;
-          },
-        };
-      }}
+    //       return <Ionicons name={iconName} />;
+    //     },
+    //   };
+    // }}
     >
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({color}) => <Icon name="home" color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name="Upload"
+        component={Upload}
+        options={{
+          tabBarIcon: ({color}) => <Icon name="cloud-upload" color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({color}) => <Icon name="person" color={color} />,
+        }}
+      />
     </Tab.Navigator>
   );
 };
