d2ca8e2407ccfdaff958221b8e6cc399137cc809
laurenthat <116711536+laurenthat@users.noreply.github.com>
2023-02-08 09:55:56 +0200

    added a button that shows the user his own uploaded files

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -9,6 +9,7 @@ import {MainContext} from '../contexts/MainContext';
 import Login from '../views/Login';
 import Upload from '../views/Upload';
 import {Icon} from '@rneui/base';
+import MyFiles from '../views/MyFiles';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -68,6 +69,7 @@ const StackScreen = () => {
             options={{headerShown: false}}
           />
           <Stack.Screen name="Single" component={Single} />
+          <Stack.Screen name="MyFiles" component={MyFiles} />
         </>
       ) : (
         <Stack.Screen name="Login" component={Login}></Stack.Screen>
